a967df06b867d135549ac19406911ca17efd5403
james heath <guruvex@gmail.com>
2018-09-04 08:56:05 -0600

    small update to URL builder

--- app/src/main/java/example/android/newsapp2/URLBuilder.java
+++ app/src/main/java/example/android/newsapp2/URLBuilder.java
@@ -7,7 +7,7 @@ package example.android.newsapp2;
  */
 
 public class URLBuilder {
-    
+
     public static final String HttpRequest = "http://content.guardianapis.com/search?show-tags=contributor&q=";
     public static final String GUARDIAN_KEY = "&api-key=a8fc710c-26a0-4c93-85b9-7319adb3f0b9";
 
